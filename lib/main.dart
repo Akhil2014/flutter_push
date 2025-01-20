1ba3cb22730a28e52fca64c6faa386a6c0076215
Akhil Khan <71203865+Akhil2014@users.noreply.github.com>
2025-01-20 21:56:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:getwidget/getwidget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void onPressed() {
+    print('Hello');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Akhil"),
+        ),
+        body:SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+              Container(child: Text('Akhil'),),
+            ],
+          ),
+        )
+    );
+  }
+}
